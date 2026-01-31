8d9ecfb01e5eba702622163a1c1f3f74a28585e9
lakeull <gurt:yo@noreply.com>
2026-01-31 00:40:23 -0600

    started it after 3 hours, thank you microsoft. i hate you.

---
+++ FrankenToilet/lakeull/itemmod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Unity;
+using UnityEngine;
+using BepInEx;
+using FrankenToilet.Core;
+
+/*
+ *   --- Public Apology ---
+ * Dear all future coders of this project.
+ * I deeply regret to inform you that the code in this project is fucking dogshit
+ * Please find it in your heart to forgive me.
+*/
+
+namespace itemMod
+{
+    [EntryPoint]
+    public static class LakeullItemMod
+    {
+        [EntryPoint]
+        public static void Awake()
+        {
+            // Plugin startup logic
+            Debug.Log("can we please go to bed now");
+        }
+    }
+}
